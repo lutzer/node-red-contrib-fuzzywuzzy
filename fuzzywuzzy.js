09d6b20d8d84b81d20bc3a30b240c4196550a49a
Lutz <lutz@Lutzs-MBP.fritz.box>
2021-02-08 18:35:05 +0100

    merged pull request

--- fuzzywuzzy.js
+++ fuzzywuzzy.js
@@ -5,39 +5,7 @@ module.exports = function(RED) {
     RED.nodes.createNode(this, config);
       const node = this;
       
-      // get content from choosen input option
-      function getInput () {
-          if (config.inputOptions === "context") {
-              let gotContext = "";
-              if (config.contextInput.length < 1) { node.error("please enter a context var"); return ""; }
-              try {
-                  switch (config.contextType) {
-                      case "flow":
-                          const flowContext = node.context().flow;
-                          gotContext = flowContext.get(config.contextInput);
-                          break;
-                        
-                      case "global":
-                          const globalContext = node.context().global;
-                          gotContext = globalContext.get(config.contextInput);
-                          break;
-                  }
-              } catch (error) {
-                  node.error(`couldnt retrieve choices from context: ${error}`);
-                  return "";
-              }
-              if (!gotContext) { node.error("please enter a valid context var"); return ""; }
-              return gotContext;
-          } else if (config.inputOptions === "text") {
-              return config.choices;
-          } else if (config.inputOptions === "message") {
-              return "";
-          }
-      }
-      
-      let input = getInput();
-      
-      // map keys to choice lines
+      // creates choice array from input string
       function makeChoices (choices) {
           const output = choices.split('\n').map( (line,i) => {
             if (line.includes(":")) {
@@ -55,11 +23,16 @@ module.exports = function(RED) {
           }).filter( (p) => p.val.length > 1) // filter empty lines
           return output;
       }
-      
-      let keyChoicePairs = makeChoices(input);
+
+      let keyChoicePairs = [];
+      try {
+        keyChoicePairs = makeChoices(config.choices);
+      } catch(err) {
+        node.error("Could not load choices")
+        return
+      }
 
       const scorer = fuzz[config.scorer]
-
       const options = {
         wildcards : "*",
         returnObjects : true,
@@ -70,23 +43,17 @@ module.exports = function(RED) {
 
       node.on('input', function(msg) {
           
-          if (msg.refresh && config.inputOptions !== "text") {
-              if (config.inputOptions !== "message") {
-                  input = getInput();
-              } else {
-                  if (typeof msg.payload !== "string") {
-                      node.error('when in "set choices with msg" mode a msg with msg.refresh set must be accompanied by the choices to be set as a single string');
-                      return;
-                  }
-                  input = msg.payload;
-              }
-              keyChoicePairs = makeChoices(input);
-              if (config.inputOptions === "context") {
-                  node.warn(`refreshed choices from ${config.contextType}.${config.contextInput}`);
-              } else if (config.inputOptions === "message") {
-                  node.warn("refreshed choices from msg.payload");
-              }
-              return;
+          if ('choices' in msg) {
+            try {
+              keyChoicePairs = makeChoices(msg.choices);
+            } catch(err) {
+              node.error("Could not load choices from msg.choices", msg)
+              return
+            }
+          }
+
+          if (typeof msg.payload !== "string") {
+            return
           }
           
           let choices = keyChoicePairs.map((p) => p.val.toLowerCase())
